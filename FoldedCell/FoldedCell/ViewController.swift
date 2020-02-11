363edbc8771dc872a8bd5ed37d2b11f2a279e53d
lourintp <thiago.lourin@objective.com.br>
2020-02-11 20:56:01 -0300

    Initial commit

---
+++ FoldedCell/FoldedCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoldedCell
+//
+//  Created by Thiago Lourin on 11/02/20.
+//  Copyright © 2020 Lourin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
