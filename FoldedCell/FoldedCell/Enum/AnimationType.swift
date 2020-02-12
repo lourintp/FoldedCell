4f2058743d7f4706e05fe6e50b3cc83ac05d12d6
lourintp <thiago.lourin@objective.com.br>
2020-02-11 21:00:31 -0300

    Creating tableviewcell file and animation types

---
+++ FoldedCell/FoldedCell/Enum/AnimationType.swift
@@ -0,0 +1,14 @@
+//
+//  AnimationType.swift
+//  FoldedCell
+//
+//  Created by Thiago Lourin on 11/02/20.
+//  Copyright © 2020 Lourin. All rights reserved.
+//
+
+import Foundation
+
+enum AnimationType {    
+    case open
+    case closed
+}
---
+++ FoldedCell/FoldedCell/FoldedTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  FoldedTableViewCell.swift
+//  FoldedCell
+//
+//  Created by Thiago Lourin on 11/02/20.
+//  Copyright © 2020 Lourin. All rights reserved.
+//
+
+import UIKit
+
+open class FoldedTableViewCell: UITableViewCell {
+
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    open override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }    
+}
